3970bfcc17ef15d2d98f925c0d4c7f840b27a05e
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-14 22:21:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,63 @@
-import { Routes, Route } from "react-router-dom";
 
-import UserLayout from "./layout/UserLayout.jsx";
-
-import Home from "./pages/Home.jsx";
-import Vendors from "./pages/Vendors.jsx";
-import Categories from "./pages/Categories.jsx";
-import Subcategories from "./pages/Subcategories.jsx";
-import Products from "./pages/Products.jsx";
-import ProductDetail from "./pages/ProductDetail.jsx";
-import Cart from "./pages/Cart.jsx";
-import Checkout from "./pages/Checkout.jsx";
-
-import UserLogin from "./pages/UserLogin.jsx";
-import UserRegister from "./pages/Register.jsx";
-
-import UserPrivateRoute from "./components/UserPrivateRoute.jsx";
+import { Routes, Route } from 'react-router-dom';
+import UserLayout from './layout/UserLayout.jsx';
+import Home from './pages/Home.jsx';
+import Vendors from './pages/Vendors.jsx';
+import Categories from './pages/Categories.jsx';
+import Subcategories from './pages/Subcategories.jsx';
+import Products from './pages/Products.jsx';
+import ProductDetail from './pages/ProductDetail.jsx';
+import Cart from './pages/Cart.jsx';
+import Checkout from './pages/Checkout.jsx';
 
 export default function App() {
   return (
     <Routes>
-
-      {/* 🔓 Public auth pages */}
-      <Route path="/login" element={<UserLogin />} />
-      <Route path="/register" element={<UserRegister />} />
-
-      {/* 🔓 Public pages */}
       <Route path="/" element={<UserLayout />}>
+
+        {/* Home */}
         <Route index element={<Home />} />
+
+        {/* Vendors list */}
         <Route path="vendors" element={<Vendors />} />
-        <Route path="vendors/:vendorId" element={<Categories />} />
+
+        {/* Categories by vendor */}
+        <Route
+          path="vendors/:vendorId"
+          element={<Categories />}
+        />
+
+        {/* Subcategories by category */}
         <Route
           path="vendors/:vendorId/categories/:categoryId"
           element={<Subcategories />}
         />
+
+        {/* Products by subcategory */}
         <Route
           path="vendors/:vendorId/categories/:categoryId/sub/:subcategoryId"
           element={<Products />}
         />
-        <Route path="product/:productId" element={<ProductDetail />} />
-        <Route path="cart" element={<Cart />} />
-      </Route>
 
-      {/* 🔒 Protected pages */}
-      <Route element={<UserPrivateRoute />}>
-        <Route path="/" element={<UserLayout />}>
-          <Route path="checkout" element={<Checkout />} />
-        </Route>
-      </Route>
+        {/* Product details */}
+        <Route
+          path="product/:productId"
+          element={<ProductDetail />}
+        />
 
+        {/* Cart */}
+        <Route
+          path="cart"
+          element={<Cart />}
+        />
+
+        {/* Checkout */}
+        <Route
+          path="checkout"
+          element={<Checkout />}
+        />
+
+      </Route>
     </Routes>
   );
 }
