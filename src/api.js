e37002ee2519264e5394afc4a4f2e95025b1977a
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-14 22:15:21 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,17 @@
+
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://myshopbackend-gj8m.onrender.com/api',
+  baseURL: 'https://myshopbackend-gj8m.onrender.com/api'
+});
+
+// Attach token for admin routes if exists
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem('adminToken');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
