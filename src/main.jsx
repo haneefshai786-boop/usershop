4b869c82743e4ea1a9fbde0e77b1f8bc84a21451
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-15 09:21:44 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
-import './styles.css';
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { CartProvider } from "./context/CartContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <CartProvider>
+    <App />
+  </CartProvider>
 );
